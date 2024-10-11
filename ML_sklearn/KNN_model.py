61a302e44b41aa67eb564435d9c4522a4b8277fe
tingjunchen425 <C112156101@nkust.edu.tw>
2024-10-12 03:17:32 +0800

    10/12

--- ML_sklearn/KNN_model.py
+++ ML_sklearn/KNN_model.py
@@ -1,3 +1,4 @@
+from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,4 +10,9 @@ x = data[['成功次數','失敗次數']]
 plt.scatter(x['成功次數'],x['失敗次數'],c=y)
 plt.xlabel('success count')
 plt.ylabel('fail count')
+
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(x,y)
+test = np.array([[112,5],[100,3],[2,50]])
+plt.scatter(test[:,0],test[:,1],c=knn.predict(test),marker='x',s=100)
 plt.show()
--- ML_sklearn/eniverment.md
+++ ML_sklearn/eniverment.md
@@ -1,4 +1,7 @@
 # eniverment
 ```
 python -m pip install scikit-learn
+ python -m pip install pandas
+ python -m pip install matplotlib
+python -m pip install openpyxl
 ```
